8199234eade65fb051a1eb6feef1b5a9fd729cfb
김자연 <jykim.developer@gmail.com>
2024-06-20 20:46:42 +0900

    feat : refactoring

--- packages/assignment-1/src/basic/RequireRefactoring.tsx
+++ packages/assignment-1/src/basic/RequireRefactoring.tsx
@@ -12,12 +12,16 @@ const PureComponent = memo(({ children, countRendering, ...props }: PropsWithChi
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 let outerCount = 1
 
+const componentStyle = { width: '100px', height: '100px' }
+
+const handleClick = () => { outerCount += 1; }
+
 // useMemo, useCallback 등을 사용하지 않고 이 컴포넌트를 개선해보세요.
 export default function RequireRefactoring({ countRendering }: Props) {
   return (
     <PureComponent
-      style={{ width: '100px', height: '100px' }}
-      onClick={() => {outerCount += 1;}}
+      style={componentStyle}
+      onClick={handleClick}
       countRendering={countRendering}
     >
       test component
